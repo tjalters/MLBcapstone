e7b92cc4bd08e666bab88a95e4e411292c2f5bb0
tjalters <tjalters32@gmail.com>
2017-04-13 14:17:28 -0500

    finished database

---
+++ MLBcapstone/MLBcapstone/Models/PlayersInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MLBcapstone.Models
+{
+    public class PlayersInfo
+    {
+        [Key]
+
+        public string playerName { get; set; }
+        public string position { get; set; }
+        public double playerRating { get; set; }
+        public double salary { get; set; }
+        public double playerValue { get; set; }
+    }
+}
---
+++ MLBcapstone/MLBcapstone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MLBcapstone.Startup))]
+namespace MLBcapstone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
